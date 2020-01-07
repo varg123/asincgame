15406e54930de8c7cb9c56d43c000df98cca7487
petrovskij465 <petrovskij465@gmail.com>
2020-01-07 18:47:51 +0500

    удалил лишнее

--- new_steps.py
+++ new_steps.py
@@ -1,48 +1,7 @@
 import time
 import curses
 import asyncio
-
-
-# документация curses  https://docs.python.org/3/library/curses.html#curses.resizeterm
-
-def draw(canvas):  # холст в качестве параметра
-    row, column = (5, 20)  # строка столбец (координаты, точка) начала текста
-    curses.curs_set(False)  # отключение мигания курсора
-    canvas.border()  # добавление рамки
-    canvas.addstr(row, column, 'Hello, World!' + str(curses.COLS))  # добавление строки
-    # canvas.addstr(row, column, 'PRESS START', curses.A_BOLD)
-    # canvas.addstr(row, column, 'PRESS START', curses.A_REVERSE | curses.A_BOLD)
-    char = canvas.getch()
-    print(f"Вы ввели {char}")
-    key = canvas.getkey()
-    print(f"Вы ввели {key}")
-
-    # Если вы введёте g, то canvas.getch() вернёт код буквы — 103. canvas.getkey() вернёт сам символ — g.
-    # Если вы нажмёте клавишу "вверх", canvas.getch() вернёт код клавиши — 259. canvas.getkey() вернёт KEY_UP.
-    # Если ввода пользователя ждать не хочется, можно вызвать функцию canvas.nodelay(True).
-
-    canvas.refresh()  # обновление холста
-    time.sleep(1)
-
-
-def draw_star(canvas):
-    row, column = (5, 20)
-    curses.curs_set(False)
-    star_char = "*"
-    while True:
-        canvas.addstr(row, column, star_char, curses.A_DIM)
-        time.sleep(2)
-        canvas.refresh()
-        canvas.addstr(row, column, star_char)
-        time.sleep(2)
-        canvas.refresh()
-        canvas.addstr(row, column, star_char, curses.A_BOLD)
-        time.sleep(2)
-        canvas.refresh()
-        canvas.addstr(row, column, star_char)
-        time.sleep(2)
-        canvas.refresh()
-
+from random import randint, choice
 
 TIC_TIMEOUT = 0.1
 
@@ -64,18 +23,7 @@ async def blink(canvas, row, column, symbol='*', timeout=TIC_TIMEOUT):
             await asyncio.sleep(0)
 
 
-def draw_asinc_star(canvas):
-    row, column = (5, 20)
-    curses.curs_set(False)
-    star_char = "*"
-    corutine_star = blink(canvas, row, column, star_char)
-    while True:
-        corutine_star.send(None)
-        canvas.refresh()
-        time.sleep(1)
-
-
-def draw_5_stars(canvas):
+def draw(canvas):
     canvas.border()
     curses.curs_set(False)
     coroutines_stars = []
@@ -92,8 +40,5 @@ def draw_5_stars(canvas):
 
 
 if __name__ == '__main__':
-    curses.update_lines_cols()  # иницализирует терминал ( записывает canvas.LINES и canvas.COLS)
-    # curses.wrapper(draw)
-    # curses.wrapper(draw_star)
-    # curses.wrapper(draw_asinc_star)
-    curses.wrapper(draw_5_stars)
+    curses.update_lines_cols()
+    curses.wrapper(draw)
